785b1ff4e91601feb06d77e8d696176a2b41e42c
EC2 Default User <ec2-user@ip-172-31-26-40.ec2.internal>
2022-02-26 03:57:53 +0000

    ユーザー一覧表示完成

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+//コントローラー(C)
+//外部ファイルの読み込み
+    require_once"User.php";
+ //物語開始
+    $kobayashi  =  new User("小林", 18);//インスタンス、オブジェクト
+    $shima = new User("島",49);
+    // //小林さんが自己紹介をする
+    // $kobayashi->hello();
+    // //島さんが自己紹介をする　　
+    // $shima->hello();
+    // //小林さんがお酒を飲む
+    // $kobayashi->drink();
+    // $shima->drink();
+    // //　小林さんが島さんに話しかける
+    // $kobayashi->talk($shima);
+    // $shima->talk($kobayashi);
+    
+    //ユーザーの一覧作成
+    $users = array();
+    $users[] = $kobayashi;
+    $users[] = $shima;
+    $users[] = new User("山田",80);
+    // var_dump($users);
+    //ユーザー一覧を表示
+    // foreach($users as $user){
+    //     print"{$user->name}\n";
+    //     print"{$user->age}歳\n";
+    //     print $user->drink();
+    // }
+    
+    //HTMLファイルを表示
+    include_once "index.view.php";
