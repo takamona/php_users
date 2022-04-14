32944af7d456656afb48b034d8687fc97db6645a
TakahisaKobayashi <takavyd2720ray@docomo.ne.jp>
2022-04-14 19:16:03 +0900

    更新

--- update_users.php
+++ update_users.php
@@ -1,23 +1,25 @@
-<?php 
+<?php
+
+
     // Controller
-    
+
     // 外部ファイルの読み込み
     require_once 'filters/csrf_filter.php';
     require_once 'models/User.php';
-    
+
     // フォームからの指定されたid値を取得
     $id = $_POST['id'];
-    
+
     // 注目してるユーザーインスタンスを取得
     $user = User::find($id);
 
     // そのようなユーザーが存在すれば
-    if($user !== false){
+    if ($user !== false) {
         // 入力データの取得
         $name = $_POST['name'];
-        $age= $_POST['age'];
+        $age = $_POST['age'];
         $gender = $_POST['gender'];
-        
+
         // インスタンス情報の更新
         $user->name = $name;
         $user->age = $age;
@@ -25,28 +27,27 @@
 
         // 入力チェック
         $errors = $user->validate();
-        
+
         // 入力エラーが1つもなければ
-        if(count($errors) === 0){
-            
+        if (count($errors) === 0) {
+
                 // データベースの更新
                 $flash_message = $user->save();
-                
+
                 // セッションにフラッシュメッセージを保存        
                 $_SESSION['flash_message'] = $flash_message;
-                
+
                 // リダイレクト
-                header('Location: show_users.php?id=' . $id);
-                exit;
-            }else{
+                header('Location: show_users.php?id='.$id);
+            exit;
+        } else {
             // セッションにエラー配列をセット
             $_SESSION['errors'] = $errors;
             // リダイレクト
-            header('Location: edit_users.php?id=' . $id);
+            header('Location: edit_users.php?id='.$id);
             exit;
         }
-        
-    }else{
+    } else {
         // セッションにエラーメッセージをセット
         $_SESSION['error'] = '存在しないユーザーです';
         // リダイレクト
